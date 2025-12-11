eb5385fa974c092a56e1e398b3f2f481754b9d20
praagyajain <155000785+praagyajain@users.noreply.github.com>
2025-12-12 02:10:31 +0530

    Time: 489 ms (51.58%), Space: 390.7 MB (48.95%) - LeetHub

---
+++ 3531-count-covered-buildings/3531-count-covered-buildings.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    int countCoveredBuildings(int n, vector<vector<int>>& buildings) {
+
+        unordered_map<int, vector<int>> rowMap;
+        unordered_map<int, vector<int>> colMap;
+
+        for (auto &b : buildings) {
+            rowMap[b[0]].push_back(b[1]);
+            colMap[b[1]].push_back(b[0]);
+        }
+
+        for (auto &r : rowMap) sort(r.second.begin(), r.second.end());
+        for (auto &c : colMap) sort(c.second.begin(), c.second.end());
+
+        int covered = 0;
+
+        for (auto &b : buildings) {
+            int x = b[0], y = b[1];
+
+            auto &row = rowMap[x];
+            auto &col = colMap[y];
+
+
+            int posRow = lower_bound(row.begin(), row.end(), y) - row.begin();
+            bool left  = (posRow > 0);
+            bool right = (posRow + 1 < row.size());
+
+
+            int posCol = lower_bound(col.begin(), col.end(), x) - col.begin();
+            bool up    = (posCol > 0);
+            bool down  = (posCol + 1 < col.size());
+
+            if (left && right && up && down)
+                covered++;
+        }
+
+        return covered;
+    }
+};
