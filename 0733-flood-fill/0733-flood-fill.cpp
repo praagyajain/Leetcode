ef6e9435b89b41538113913382f4efa4894f7ed7
praagyajain <155000785+praagyajain@users.noreply.github.com>
2026-08-15 17:24:17 +0530

    Time: 0 ms (100%), Space: 18.4 MB (30.75%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -3,31 +3,31 @@ public:
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
         int originalColor = image[sr][sc];
         if (originalColor == color) return image;
-        queue<pair<int,int>> q;
-        q.push({sr,sc});
-        int m = image.size();
-        int n = image[0].size();
-        vector<vector<int>> copy(m,vector<int> (n,0));
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
+        int n = image.size();
+        int m = image[0].size();
+        vector<vector<int>> copy(n,vector<int>(m,0));
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
                 copy[i][j] = image[i][j];
             }
         }
         copy[sr][sc] = color;
-        while(!q.empty()){
-                    int delrow[] = {-1,0,1,0};
-        int delcol[] = {0,1,0,-1};
 
-            int r = q.front().first;
-            int c = q.front().second;
-            q.pop();
+        queue<pair<int,int>> q;
+        q.push({sr,sc});
+        while(!q.empty()){ 
+           int a =  q.front().first;
+           int b =  q.front().second;
+           q.pop();
+           int delrow[4] = {-1,0,1,0};
+           int delcol[4] = {0,1,0,-1};
            for(int i=0;i<4;i++){
-               int nrow = r+delrow[i];
-               int ncol = c+delcol[i];
-                if(nrow>=0 && nrow<m && ncol>=0 && ncol<n && copy[nrow][ncol]==image[sr][sc]){
-                   q.push({nrow,ncol});
-                   copy[nrow][ncol] = color;
-               }
+            int r = a + delrow[i];
+            int c = b + delcol[i];
+            if(r<n && r>=0 && c<m && c>=0 && copy[r][c] == image[a][b]){
+                q.push({r,c});
+                copy[r][c] = color;
+            }
            }
         }
         return copy;
